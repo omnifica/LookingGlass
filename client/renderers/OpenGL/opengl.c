8e30f5d347a044c3dcdc1949755216cc7abe81ed
omnifica <64203873+omnifica@users.noreply.github.com>
2020-04-23 07:12:54 -0500

    [client] opengl: render frame if config didn't change

--- client/renderers/OpenGL/opengl.c
+++ client/renderers/OpenGL/opengl.c
@@ -564,6 +564,8 @@ bool opengl_render(void * opaque, SDL_Window * window)
       return false;
 
     case CONFIG_STATUS_NOOP :
+      break;
+
     case CONFIG_STATUS_OK   :
      if (!draw_frame(this))
        return false;
